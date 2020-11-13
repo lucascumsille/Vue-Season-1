656bf77c325cdde4a88b8f5571a02740aad5911b
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-11-13 14:23:15 +1100

    L8-end

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const app = Vue.createApp({
             details: ['50% cotton', '30% wool', '20% polyester'],
             selectedVariant: 0,
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale:true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
             ]
         }
     }, // data  
@@ -30,6 +30,9 @@ const app = Vue.createApp({
         },
         inStock () {
             return this.variants[this.selectedVariant].quantity;
+        }, 
+        onSale () {
+            return this.variants[this.selectedVariant].onSale;
         }
     } // computed
 })
